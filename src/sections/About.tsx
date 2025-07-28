1f3574109ec067a9dabfc9ca332af918aed0c617
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2025-07-27 21:26:47 -0300

    feat: update About section with new introduction and improve styling

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -26,28 +26,26 @@ const About = () => {
             <Row className="about-row">
 
                 <Col md={8} className="about-text-col">
-                    <div className="intro-text">
-                        <p>Hi! My name is <span className="highlight">Milena</span> and I'm a <span className="highlight">software developer</span>.</p>
-                        
-                        <p>
-                        I have experience in <span className="highlight">full-stack development</span>, especially 
-                        with <span className="highlight">Laravel</span>, <span className="highlight">React</span>, and <span className="highlight">Spring Boot</span>, building applications 
-                        that are both <span className="highlight">efficient</span> and <span className="highlight">user-friendly</span>. 
-                        Currently, I work at the <span className="highlight">State Health Department</span>, where I 
-                        develop and maintain systems that improve healthcare 
-                        management.
-                        </p>
-                        
-                        <p>
-                        I also have experience with <span className="highlight">APIs</span>, <span className="highlight">AI</span>, relational and non-
-                        relational databases, <span className="highlight">web design</span>, and <span className="highlight">mobile 
-                        development</span> using <span className="highlight">Flutter</span>. Besides my job, I enjoy 
-                        exploring new <span className="highlight">technologies</span>, working on personal 
-                        projects, and collaborate to projects, such as <span className="highlight">Hema</span>, a 
-                        mobile app I developed for managing treatments for 
-                        leprosy.
-                        </p>
-                    </div>
+                <div className="intro-text">
+                  <p>
+                    Olá! Me chamo Milena Neves, <span className="highlight">desenvolvedora de software</span> com formação em <span className="highlight">Ciência e Tecnologia</span> e atualmente estudante de <span className="highlight">Engenharia da Computação</span>.
+                    Tenho experiência no desenvolvimento de aplicações <span className="highlight">web</span> e <span className="highlight">mobile</span>, 
+                    com foco em <span className="highlight">back-end</span>, utilizando tecnologias como <span className="highlight">Laravel</span>, <span className="highlight">Spring Boot</span>, <span className="highlight">React</span> e <span className="highlight">Flutter</span>.
+                  </p>
+
+                  <p>
+                    Atuo em todas as etapas do <span className="highlight">ciclo de desenvolvimento de software</span> desde a <span className="highlight">modelagem de dados</span> até a implementação de <span className="highlight">funcionalidades</span> e  <span className="highlight">interfaces</span>. Embora meu foco principal seja o <span className="highlight">back-end</span>, 
+                    gosto de criar e aprimorar o <span className="highlight">design</span> das aplicações sempre que necessário, 
+                    buscando entregar soluções <span className="highlight">completas</span>, <span className="highlight">funcionais</span> e com <span className="highlight"> boa experiência para o usuário</span>.
+                  </p>
+
+                  <p>
+                    Atualmente, estou buscando me aprofundar em <span className="highlight">arquitetura de software</span> e <span className="highlight">inteligência artificial</span>. Gosto de <span className="highlight">trabalhar em equipe,</span> <span className="highlight">aprender novas tecnologias</span> e enfrentar <span className="highlight">desafios</span> que me façam 
+                    crescer como profissional e como pessoa.
+                  </p>
+                </div>
+
+
                 </Col>
 
                 
